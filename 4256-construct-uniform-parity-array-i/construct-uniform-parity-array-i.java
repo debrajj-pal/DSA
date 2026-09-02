e887d2ccb8bafa8b469ac10e5bfc68d22c82f22b
Debraj Pal <debraj4990@gmail.com>
2026-09-02 17:59:14 +0530

    Time: 1 ms (31.89%) | Memory: 45 MB (87.60%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int odd=0;
+        int even=0;
+        for(int num: nums1){
+            if(num%2==0){
+                even++;
+            }else{
+                odd++;
+            }
+        }
+        return true;
+        
+    }
+}
